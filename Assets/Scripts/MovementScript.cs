943ea50095ec6389a9865a5d17a1799540e73445
Tormod2 <vova.dekterev@mail.ru>
2021-03-24 15:08:55 +0700

    Added some movement scripts and some materials

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0,0,100);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -100);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(100, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-100, 0, 0);
+        }
+    }
+}

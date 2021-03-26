b7c9f8d1344d85c9401e39e11c6a632a20ffa20b
Tormod2 <vova.dekterev@mail.ru>
2021-03-26 23:18:39 +0700

    Added some proper movement,started doing dropbox thingy

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -1,35 +1,85 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using Dropbox.Api;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class MovementScript : MonoBehaviour
 {
+    
+    public Transform tr;
+    public LineRenderer lr;
+    private float _time;
+    private List<Vector3> _trajectory;
+    private bool _loop;
+    private string _bodyJsonString;
 
-    public Rigidbody rb;
-
-    void Start()
+    private IEnumerator Start()
     {
+        string token = "sl.Att8NwrzTVNSFlRuBryu0s9qUBnr1kti34CPlURfnhtUaXbcGfp4FeEnFYtQyNPZhusqgKDvvIOf_NMVmFaZQ1xCaJKGUg1iE2wWCQ5rt_w0Em5vznFIRxCCFzUgotPZndoUnoU";
+        Sequence mySequence = DOTween.Sequence();
+
         
+
+        //Dictionary<string, string> postHeader = new Dictionary<string, string>();
+        //postHeader.Add("Authorization", "Bearer " + token);
+        //postHeader.Add("Dropbox-API-Arg", "{\"path\": \"/provaupload.json\"}");
+        //postHeader.Add("Content-Type", "");
+        //using (UnityWebRequest request = UnityWebRequest.Get("https://www.dropbox.com/sh/2bym9ochyew91e5/AAAeZgiDflL1z_De1-mEZ9qTa?dl=0/Data.json"))
+        ////"https://content.dropboxapi.com/2/files/download", null, postHeader))
+        //{
+        //    //request.SetRequestHeader("Authorization", "Bearer " + token);
+            
+        //    yield return request.SendWebRequest();
+        //    if (request.error != null)
+        //    {
+        //        Debug.Log(request.error + " " + request.error);
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("Success! " + request.downloadHandler.text);
+        //    }
+        //}
+        yield return null;
+
+
+
+        //lr.positionCount = _trajectory.Count;
+
+        //await Task.Run( GetDropBoxFile);
+        //foreach (var t in _trajectory)
+        //{
+        //    mySequence.Append(transform.DOMove(t, _time / _trajectory.Count));
+        //    lr.SetPosition(_trajectory.IndexOf(t), t);                      
+        //}
+
+        //DOTween.Play(mySequence);
+
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (Input.GetKey("w"))
-        {
-            rb.AddForce(0,0,100);
-        }
-        if (Input.GetKey("s"))
-        {
-            rb.AddForce(0, 0, -100);
-        }
-        if (Input.GetKey("d"))
-        {
-            rb.AddForce(100, 0, 0);
-        }
-        if (Input.GetKey("a"))
-        {
-            rb.AddForce(-100, 0, 0);
-        }
-    }
+
+    //static async Task GetDropBoxFile()
+    //{
+    //    string token = "sl.Att8NwrzTVNSFlRuBryu0s9qUBnr1kti34CPlURfnhtUaXbcGfp4FeEnFYtQyNPZhusqgKDvvIOf_NMVmFaZQ1xCaJKGUg1iE2wWCQ5rt_w0Em5vznFIRxCCFzUgotPZndoUnoU";
+    //    using (var dbx = new DropboxClient(token))
+    //    {
+    //        using (var response = await dbx.Files.DownloadAsync("Test" + "/" + "Data.json"))
+    //        {
+    //            Debug.Log(await response.GetContentAsStringAsync());
+    //        }
+
+
+    //    }
+    //}
+    
+
+
+
 }
+
+
